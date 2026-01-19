e0136d4ad5ed882570155537a1473945af6c9d87
Himanshu Tiwary <149292671+Himanshu2k24@users.noreply.github.com>
2026-01-20 02:56:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import threading
+import bot   # this imports bot.py
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
+@app.route("/")
+def home():
+    return "Bot is alive"
 
+def start_bot():
+    bot.main()   # your bot start function
 
 if __name__ == "__main__":
-    app.run()
+    threading.Thread(target=start_bot).start()
+    app.run(host="0.0.0.0", port=10000)
